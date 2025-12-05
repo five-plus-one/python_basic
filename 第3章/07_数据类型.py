17086d7c5bc6d83e119eaba9bd3547c8abd82668
five-plus-one <5plus1@five-plus-one.com>
2025-12-05 19:21:32 +0800

    Initial commit

---
+++ 第3章/07_数据类型.py
@@ -0,0 +1,6 @@
+print(type('张三'))
+print(type(18))
+print(type(65.2))
+
+print("*****")
+print(type('18'))
---
+++ 第3章/08_整型.py
@@ -0,0 +1,12 @@
+age = 18
+temp = -15
+score = 0
+
+salary = 300_000
+house_price = 3_200_000
+graduates = 12_000_000
+print(salary, house_price, graduates)
+
+a = 9 ** 999
+# print(a)
+b = a + 100
