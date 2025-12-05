5fd9c6187a38a9f68a60d1d074de79ae32240690
five-plus-one <5plus1@five-plus-one.com>
2025-12-05 19:24:47 +0800

    08

--- 第3章/08_整型.py
+++ 第3章/08_整型.py
@@ -1,12 +1,18 @@
+import sys
+
 age = 18
 temp = -15
 score = 0
 
+# 当数很大时，可以用下划线进行分组
 salary = 300_000
 house_price = 3_200_000
 graduates = 12_000_000
 print(salary, house_price, graduates)
 
+# Python中整数的上限值，取决于执行代码的计算机的内存和处理能力
 a = 9 ** 999
 # print(a)
 b = a + 100
+sys.set_int_max_str_digits(0)  # 解除数字转字符串长度限制
+print(a)
