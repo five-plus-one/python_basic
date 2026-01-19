bbd3a022d5ef09849047e558a2b0feee23246773
five-plus-one <5plus1@five-plus-one.com>
2026-01-19 19:17:05 +0800

    6.3

--- 第6章/03_列表_增删改查.py
+++ 第6章/03_列表_增删改查.py
@@ -1,8 +1,8 @@
 # 新增操作
 # 方式一：通过列表的append方法，在列表尾部追加一个元素
-nums = [10, 20, 30, 40]
-nums.append(50)
-print(nums)
+# nums = [10, 20, 30, 40]
+# nums.append(50)
+# print(nums)
 
 # 方式二：通过列表的insert方法，在列表的指定下标处添加一个元素
 # nums = [10, 20, 30, 40]
@@ -18,10 +18,10 @@ print(nums)
 
 # 删除操作
 # 方式一：通过列表的pop方法，删除指定位置的元素，并返回该元素
-# nums = [10, 20, 10, 40, 50]
-# result = nums.pop(1)
-# print(nums)
-# print(result)
+nums = [10, 20, 10, 40, 50]
+result = nums.pop(1)
+print(nums)
+print(result)
 
 # 方式二：通过列表的remove方法，删除列表中第一次出现的指定值
 # nums = [10, 20, 10, 40, 50]
